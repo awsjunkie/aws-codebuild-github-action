e9e6461d2305305339176e0e2ac83adb033c8993
AwsJunkie <100114557+awsjunkie@users.noreply.github.com>
2023-07-10 21:22:55 -0400

    Add files via upload

---
+++ src/main/java/com/awsjunkie/codebuild/GitHubActionTest.java
@@ -0,0 +1,8 @@
+package com.awsjunkie.codebuild;
+
+public class GitHubActionTest {
+	private static final String text = "asgaG";
+	public static void main(String[] args) {System.out.println("GitHub Action Test...");}
+	
+	private void METHODNAME() {}
+}
